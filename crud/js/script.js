a3f1b59d5f137740f561254d45e5b6355bcd6d2e
Biruk Kebede <61163684+birukkebede11@users.noreply.github.com>
2022-05-11 15:19:49 -0400

    Update and Delete form clears the value after submission

--- crud/js/script.js
+++ crud/js/script.js
@@ -1,6 +1,6 @@
 function formSubmit(e) {
 	e.preventDefault();
-	return fetch("http://localhost:1234/update", {
+	fetch("http://localhost:1234/update", {
 		method: "PUT",
 		headers: { "content-type": "application/json" },
 		body: JSON.stringify({
@@ -10,6 +10,8 @@ function formSubmit(e) {
 	})
 		.then((response) => response.json())
 		.then((data) => alert("Name Updated!"));
+
+	document.getElementById("myForm").reset();
 }
 
 document.getElementById("myForm").addEventListener("submit", formSubmit);
@@ -17,13 +19,15 @@ document.getElementById("myForm").addEventListener("submit", formSubmit);
 function deleteUser(e) {
 	e.preventDefault();
 	alert("User Deleted!");
-	return fetch("http://localhost:1234/remove-user", {
+	fetch("http://localhost:1234/remove-user", {
 		method: "DELETE",
 		headers: { "content-type": "application/json" },
 		body: JSON.stringify({
 			id: document.querySelector("#delete-user input[name=id]").value,
 		}),
 	});
+
+	document.getElementById("delete-user").reset();
 }
 
 document.getElementById("delete-user").addEventListener("submit", deleteUser);
